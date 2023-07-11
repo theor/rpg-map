2998e44604b1e19d2802acc0981a17f66441e022
Theo Richart <theor@unity3d.com>
2023-07-11 12:58:36 -0400

    fix zoom and bounds

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,22 @@ import L from 'leaflet';
 
 L.Icon.Default.imagePath = 'img/icon/';
 
+const F = 2;
 const map = L.map('map', {
-    center: [20,20],
+    // center: [20,20],
     crs: L.CRS.Simple,
     // minZoom: -5,
     // layers: [m_mono],
-}).setView([0.0, 0.0], 4)
-//.setMaxBounds(L.latLngBounds(L.latLng(0, 0), L.latLng(1000, 100)));;
+}).setView([-64.0, 128.0], 2)
+// .setMaxBounds(L.latLngBounds(L.latLng(-164*F, -128*F), L.latLng(164*F, 128*F)));;
 L.tileLayer('{z}/{x}.{y}.webp', {
-    maxNativeZoom:4,
+    maxNativeZoom:7,
     minZoom: 1,
     maxZoom: 8,
     noWrap: true,
 
 }).addTo(map);
-
+console.log(map.getBounds())
 
 L.control.scale({
     imperial: false,
